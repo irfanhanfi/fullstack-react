cc5ac0e1b4c5700f6a1a6b202e85709ca373bfda
irfan-synerzip <irfan.hanfi@synerzip.com>
2018-08-21 14:39:50 +0530

    private routes

--- routing/music/client/src/components/App.js
+++ routing/music/client/src/components/App.js
@@ -3,6 +3,9 @@ import { Route, Redirect } from 'react-router-dom';
 
 import TopBar from './TopBar';
 import AlbumsContainer from './AlbumsContainer';
+import Login from './Login';
+import Logout from './Logout';
+import PrivateRoute from './PrivateRoute';
 
 import '../styles/App.css';
 
@@ -11,7 +14,10 @@ const App = () => (
     <TopBar />
     <div className='spacer row' />
     <div className='row'>
-      <Route path='/albums' component={AlbumsContainer} />
+      <PrivateRoute path='/albums' component={AlbumsContainer} />
+      <Route path='/login' component={Login} />
+      <Route path='/logout' component={Logout} />
+
       <Route exact path='/' render={() => (
         <Redirect
           to='/albums'
