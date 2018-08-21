3dfd1033e6a49d25b0ee2f982c32dfa41585ed3a
irfan-synerzip <irfan.hanfi@synerzip.com>
2018-08-21 11:22:45 +0530

    init commit

---
+++ routing/music/client/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import TopBar from './TopBar';
+import AlbumsContainer from './AlbumsContainer';
+
+import '../styles/App.css';
+
+const App = () => (
+  <div className='ui grid'>
+    <TopBar />
+    <div className='spacer row' />
+    <div className='row'>
+      <Route path='/albums' component={AlbumsContainer} />
+      <Route exact path='/' render={() => (
+        <Redirect
+          to='/albums'
+        />
+      )} />
+    </div>
+  </div>
+);
+
+export default App;
---
+++ voting_app/public/js/app.js
@@ -0,0 +1,119 @@
+class Product extends React.Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.handleUpVote = this.handleUpVote.bind(this);
+    // }
+
+    render() {
+
+        return (
+            <div className='item'>
+                <div className='image'>
+                    <img src={this.props.productImageUrl} />
+                </div>
+                <div className='middle aligned content'>
+                    <div className='header'>
+                        <a onClick={this.handleUpVote}>
+                            <i className='large caret up icon' />
+                        </a>
+                        {this.props.votes}
+                    </div>
+                    <div className='description'>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
+                        <p>
+                            {this.props.description}
+                        </p>
+                    </div>
+                    <div className='extra'>
+                        <span>Submitted by:</span>
+                        <img
+                            className='ui avatar image'
+                            src={this.props.submitterAvatarUrl}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    handleUpVote = () => {
+        this.props.onVote(this.props.id);
+    }
+}
+
+class ProductList extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         products: [],
+    //     };
+
+    //     this.handleProductUpVote = this.handleProductUpVote.bind(this);
+    // }
+
+    state = {
+        products: [],
+    };
+
+    componentDidMount() {
+        this.setState({ products: Seed.products });
+    }
+
+    renderProduct(product) {
+        return (
+            <Product
+                key={'product-' + product.id}
+                id={product.id}
+                title={product.title}
+                description={product.description}
+                url={product.url}
+                votes={product.votes}
+                submitterAvatarUrl={product.submitterAvatarUrl}
+                productImageUrl={product.productImageUrl}
+                onVote={this.handleProductUpVote}
+            />
+        );
+    }
+
+    render() {
+        const products = this.state.products.sort((a, b) => (
+            b.votes - a.votes
+        ));
+
+        return (
+            <div className='ui unstackable items'>
+                {products.map(product => this.renderProduct(product))}
+            </div>
+        );
+    }
+
+    handleProductUpVote = (productId) => {
+        const nextProducts = this.state.products.map((product) => {
+            if (product.id === productId) {
+                return Object.assign({}, product, {
+                    votes: product.votes + 1,
+                });
+            } else {
+                return product;
+            }
+        });
+
+        this.setState({
+            products: nextProducts,
+        });
+    }
+}
+
+ReactDOM.render(
+    <ProductList />,
+    document.getElementById('content')
+);
+
+
+
+// ReactDOM.render(
+//     // ...
+// );
